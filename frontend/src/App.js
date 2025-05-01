50370b2483c3f48af086c0298a8e3848dda64ff6
VasupriyaPatnaik <vasupriyab004@gmail.com>
2025-05-01 14:39:51 +0530

    Add Navbar and Footer routes to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Navbar from "./components/Navbar"; 
+import Footer from "./components/Footer"; 
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-100 text-gray-900">
         <Routes>
+          <Route path="/navbar" element={<Navbar />} />
+          <Route path="/footer" element={<Footer />} />
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
